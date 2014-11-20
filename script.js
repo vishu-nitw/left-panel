f40c03dff4c63a2f635881472c140614337ca7f3
unknown <puviswan@DIN67000301.corp.capgemini.com>
2014-11-20 13:25:04 +0530

    modified script.js

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-$(function () {
+$(document).ready(function () {
     $('#tabs')
     .tabs()
     .addClass('ui-tabs-vertical ui-helper-clearfix');
+
+    customerData();
   });
+
+function customerData(){
+	$.ajax({
+		url : "customer.xml",
+		dataType : "xml",
+		success : function(data) {
+
+			$(data).find("employee").each(function(){
+
+				var addInfo ='<li>Name: '+$(this).find("name").text()+'</li>'
+                       		'<li>Age: '+$(this).find("age").text()+'</li>'
+                       		'<li>Company: '+$(this).find("company").text()+'</li>';
+ 
+			  				$("ul").append(addInfo);
+			});
+		},
+		error: function() { 
+			$("ul").children().remove(); 
+			$("ul").append("<li>There was an error in ur code!</li>"); 
+		}
+	});
+}
